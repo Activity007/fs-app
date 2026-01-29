3e915798a5de1e9937588359daef7ca501cbcf3d
Activity007 <tithcholna742@gmail.com>
2026-01-29 11:50:11 +0700

    add dark mode/ skeleton

---
+++ app/product/loading.tsx
@@ -0,0 +1,16 @@
+import { SkeletonCard } from "@/components/skeletons/skeleton-card";
+
+export default function ProductLoading() {
+
+  return (
+    <main className="container mx-auto">
+      <section className="grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-6 gap-4 m-2">
+        {
+            Array.from({ length: 15 }).map((_, index) => (
+                <SkeletonCard key={index} />
+            ))
+        }
+      </section>
+    </main>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "./mode-toggle";
+
+export default function Header(){
+    return(
+        <div className="flex justify-start">
+            <ModeToggle />
+        </div>
+    )
+}
